cbf145668526ed9f98d8f0eb52b7b188b9123c70
Walter Joy IV <waltoids@users.noreply.github.com>
2020-05-22 20:16:18 -0400

    added functions to make nine rows as once instead making them each by hand

--- Java/script.js
+++ Java/script.js
@@ -1,5 +1,17 @@
-const currentDay = $("#currentDay");
-const container = $(".constainer");
+$(document).ready(function (){
+  const currentDay = $("#currentDay");
+  const container = $(".constainer");
 
-//Set the date in header
-currentDay.text(moment().format("dddd, MMMM Do")); 
+  //Set the date in body / header / p tag
+  currentDay.text(moment().format("[Today is] dddd, MMMM Do YYYY, h:mm a")); 
+
+  //Need to create 9 rows, all similar in structure, for loop is a good way to accomplish this
+  for (let i = 0; i < 9; i++) {
+      createRows(i);
+  }
+  // create function to creates a row/input field for user/ save button to save locally
+  function createRows (num) {
+      const rows = $(`<div class = "row" id = ${num}>`);
+  }
+
+});
